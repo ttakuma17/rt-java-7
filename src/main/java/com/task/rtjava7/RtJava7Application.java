00fd9b9b2f4c60a6c771ab73116c6c1190c6d6c5
ttakuma17 <ttanimiya@gmail.com>
2022-12-29 13:06:42 +0900

    第7回課題用プロジェクト

---
+++ src/main/java/com/task/rtjava7/RtJava7Application.java
@@ -0,0 +1,13 @@
+package com.task.rtjava7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtJava7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtJava7Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/rtjava7/RtJava7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.rtjava7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtJava7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
